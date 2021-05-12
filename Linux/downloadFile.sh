72ce03a05e65baeb03370baa0045bbb0b0b4042e
ShyamRayasam <sunderrs2004@yahoo.com>
2021-05-12 20:12:20 +0530

    Added Comments

--- Linux/downloadFile.sh
+++ Linux/downloadFile.sh
@@ -1,6 +1,16 @@
+echo "Downloading Zip File"
 wget https://github.com/glowroot/glowroot/releases/download/v0.13.6/glowroot-0.13.6-dist.zip
+echo "Unzipping zip file"
 unzip -q glowroot-0.13.6-dist.zip
+echo "List contents of Zip file"
 unzip -l glowroot-0.13.6-dist.zip
+echo "List contents in glowroot folder"
 ls -lhrt glowroot
+echo "Removing files with extension .jar in glowroot folder"
 rm glowroot/*.jar
+echo "Listing contents of glowroot folder"
+ls -lhrt glowroot
+echo "Removing files with extension .jar in glowroot/lib folder"
 rm glowroot/lib/*.jar
+echo "Listing contents of glowroot/lib folder"
+ls -lhrt glowroot/lib
