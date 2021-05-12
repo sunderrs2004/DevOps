dd24a59c2a7c6cb0d3659d1607458f50447d7a3d
ShyamRayasam <sunderrs2004@yahoo.com>
2021-05-12 19:52:35 +0530

    Committing Version 1 of the Requirement

--- Linux/downloadFile.sh
+++ Linux/downloadFile.sh
@@ -1,2 +1,6 @@
-ls -lhrt
-ls -al
+wget https://github.com/glowroot/glowroot/releases/download/v0.13.6/glowroot-0.13.6-dist.zip
+unzip -q glowroot-0.13.6-dist.zip
+unzip -l glowroot-0.13.6-dist.zip
+ls -lhrt glowroot
+rm glowroot/*.jar
+rm glowroot/lib/*.jar
